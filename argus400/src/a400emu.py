bd83444bc98f5f16bcc1e15d7bdd4010e6a04596
revaldinho <richevans@mac.com>
2025-06-18 21:02:30 +0100

    Add instruction timings for Argus 500 ; add switches for different machine types

--- argus400/src/a400emu.py
+++ argus400/src/a400emu.py
@@ -26,44 +26,142 @@ REQUIRED SWITCHES ::
 
   -f --filename  <filename>      specify the assembler source file
 
+OPTIONAL SWITCHES ::
+
+  -n --nolisting                 turn off the debug trace output
+
+  -1 --100                       emulate only Argus 100/400 instructions
+
+  -4 --400                       emulate only Argus 100/400 instructions
+
+  -5 --500                       emulate Argus 500 instructions and print an instruction
+                                 timing summary at the end of emulation (default)
+
+  -h --help                      print this help message
+
 EXAMPLES :
 
   python3 a400asm.py -f test.hex
 
 '''
 from functools import reduce
+from operator import add
+
 import sys, re, getopt
+import datetime
+
+op = {
+    "ldx":0x0, "nlx":0x1, "add":0x2, "sub":0x3,
+    "ldc":0x4, "lmc":0x5, "adc":0x6, "sbc":0x7,
+    "sto":0x8, "stn":0x9, "ads":0x0A, "ssb":0x0b,
+    "exc":0xc, "and":0xd, "neq":0x0e, "orf":0x0f,
+    "jze":0x10, "jnz":0x11, "jge":0x12, "jlt":0x13,
+    "ovr":0x14, "jbs":0x15, "out":0x16, "jcs":0x17,
+    "sra":0x18, "sla": 0x19, "srl": 0x1a, "slc":0x1b,
+    "sll":0x1c, "slv":0x1d,
+    "mpy":0x1e, "div": 0x1f
+}
+
+a500_model_id = ("Series 1, Model 1", "Series 2, Model 2", "Series 3, Model 1", "Series 4, Model 2")
+# Instruction times per model taken from Argus 500 training manual
+#              :  S1M1 S2M2 S3M1 S4M2
+a500_timing_us = { op["ldx"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["nlx"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["add"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["sub"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["ldc"] : ( 4.7, 3.1, 4.7, 2.8 ),
+                   op["lmc"] : ( 4.7, 3.1, 4.7, 2.8 ),
+                   op["adc"] : ( 4.7, 3.1, 4.7, 2.8 ),
+                   op["sbc"] : ( 4.7, 3.1, 4.7, 2.8 ),
+                   op["sto"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["stn"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["ads"] : ( 6.4, 4.0, 6.4, 3.6 ),
+                   op["ssb"] : ( 6.4, 4.0, 6.4, 3.6 ),
+                   op["exc"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["and"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["neq"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["orf"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["jze"] : ( 4.0, 2.4, 4.0, 2.2 ),
+                   op["jnz"] : ( 4.0, 2.4, 4.0, 2.2 ),
+                   op["jge"] : ( 4.0, 2.4, 4.0, 2.2 ),
+                   op["jlt"] : ( 4.0, 2.4, 4.0, 2.2 ),
+                   op["ovr"] : ( 2.7, 1.9, 3.4, 1.9 ),
+                   op["jbs"] : ( 2.7, 1.9, 3.4, 1.9 ),
+                   op["out"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["jcs"] : ( 6.0, 3.6, 6.0, 3.2 ),
+                   op["sra"] : ( 4.4, 2.8, 4.4, 2.6 ),
+                   op["sla"] : ( 4.4, 2.8, 4.4, 2.6 ),
+                   op["srl"] : ( 4.4, 2.8, 4.4, 2.6 ),
+                   op["slc"] : ( 4.4, 2.8, 4.4, 2.6 ),
+                   op["sll"] : ( 4.4, 2.8, 4.4, 2.6 ),
+                   op["slv"] : ( 6.4, 4.0, 6.4, 3.6 ),
+                   op["mpy"] : ( 13.4, 11.5, 13.4, 11.1),
+                   op["div"] : ( 15.0, 13.1, 15.0, 12.7)
+                  }
+## Add to basic timing for reading from IO space
+a500_IO_inc_us   = ( 2.0, 1.2, 2.0, 1.1 )
+## Add to basic timing for modified address operations (ie modifier != 0 )
+a500_timing_mod_us = ( 2.0, 1.2, 2.0, 1.1 )
+# Add this factor * N for multiplace shift operations
+a500_shiftperbit_us= 0.4
+
+def a500_exec_time_us ( opcode, operand, modifier, io_low, io_high, memstore ) :
+    t_us = [0]* 4
+    # NB operand has already been modified as required for computing the shift distance or determining IO address
+    shift_increment = a500_shiftperbit_us * (operand % 32) if (op["sra"] <= opcode <= op["slv"]) else 0
+    for i in range (0, 4):
+        t_us[i] = a500_timing_us[opcode][i] + shift_increment
+        t_us[i] += a500_IO_inc_us[i] if ( io_low <= operand <= io_high ) else 0
+        t_us[i] += a500_timing_mod_us[i] if ( modifier > 0 ) else 0
+    return t_us
+
+def print_exec_time ( t ) :
+    print ( "Nominal execution times for Argus 500 models")
+    series = 0
+    model = 0
+    for i in t:
+        if i > 10000000 :
+            years = divmod ( i/1000000, 31536000) # get years
+            weeks = divmod ( years[1], 604800)  # use remainder from here on
+            days = divmod ( weeks[1], 86400)
+            hours = divmod ( days[1], 3600)
+            minutes = divmod ( hours[1], 60)
+            seconds = divmod (minutes[1],1)
+            print ( "- %s : %s%s%s%s" % ( a500_model_id[series],
+                                          "%d years " % years[0] if years[0] >0 else "",
+                                          "%d weeks " % weeks[0] if weeks[0] >0 else "",
+                                          "%d minutes " % minutes[0] if minutes[0] >0 else "",
+                                          "%d seconds" % seconds[0] if seconds[0] > 0 else ""))
+        elif i > 1000:
+            print ( "- %s : %8.4f milliseconds" % ( a500_model_id[series], i/1000))
+        else:
+            print ( "- %s : %8.1f microseconds" % ( a500_model_id[series], i))
+        series+=1
+
 
 def usage():
     print (__doc__);
     sys.exit(1)
 
 def readhex( filename ) :
-    with open(filename,"rt") as f:
-        wordmem = [ (int(x,16) & 0xFFFFFF) for x in f.read().split() ]
-    wordmem.extend( [0] * (16384  - len(wordmem) ))
-    return wordmem
+    try:
+        with open(filename,"rt") as f:
+            wordmem = [ (int(x,16) & 0xFFFFFF) for x in f.read().split() ]
+            wordmem.extend( [0] * (16384  - len(wordmem) ))
+        return wordmem
+    except:
+        print ( "Error reading %s" % filename )
+        sys.exit(1)
 
+def emulate ( filename, nolisting, machine ) :
 
-def emulate ( filename, nolisting ) :
-
-    op = {
-        "ldx":0x0, "nlx":0x1, "add":0x2, "sub":0x3,
-        "ldc":0x4, "lmc":0x5, "adc":0x6, "sbc":0x7,
-        "sto":0x8, "stn":0x9, "ads":0x0A, "ssb":0x0b,
-        "exc":0xc, "and":0xd, "neq":0x0e, "orf":0x0f,
-        "jze":0x10, "jnz":0x11, "jge":0x12, "jlt":0x13,
-        "ovr":0x14, "jbs":0x15, "out":0x16, "jcs":0x17,
-        "sra":0x18, "sla": 0x19, "srl": 0x1a, "slc":0x1b,
-        "sll":0x1c, "slv":0x1d,
-        "mpy":0x1e, "div": 0x1f
-    }
     dis = dict( [ (op[k],k) for k in [ x for x in op ]])
     reg = {"Z":0x0000, "R":0x0001, "Q":0x0002, "C":0x003, "HSW":0x0004,
            "INPUT":0x1000, "LINK":0x1008, "INT":0x1010}
 
     wordmem = readhex( filename )
 
+    timers = [0.0,0.0,0.0,0.0]
     conout = []
     (ovr, busy, pc, instr_count) = (0, 0, 0x1020, 0) # initialise machine state inc PC
 
@@ -97,6 +195,9 @@ def emulate ( filename, nolisting ) :
             # print ( "MEM: " + " ".join( [ "%06x" % (wordmem[i]&0xFFFFFF) for i in range ( 0x1100, 0x1110)]))
 
         pc += 1
+
+        if machine == 500:
+            timers = [ sum(i) for i in zip(timers, a500_exec_time_us( opcode, operand, mod, 0x010, 0x1000, wordmem ) ) ]
 
         if opcode == op["ldx"]:
             result = wordmem[operand]
@@ -218,11 +319,19 @@ def emulate ( filename, nolisting ) :
             result = ((double << operand & 0x1F) >> 48)
             wordmem[acc_adr] = result & 0xFFFFFF
         elif opcode == op["sll"]:
-            # FIXME - A500 only
-            pass
-        elif opcode == op["slc"]:
-            # FIXME - A500 only
-            pass
+            if machine == 500:
+                # FIXME - A500 only
+                pass
+            else:
+                print("Error - opcode %d is not implemented in Argus 100 or 400 machines, SLL in Argus 500")
+                sys.exit(1)
+        elif opcode == op["slv"]:
+            if machine == 500:
+                # FIXME - A500 only
+                pass
+            else:
+                print("Error - opcode %d is not implemented in Argus 100 or 400 machines, SLV in Argus 500")
+                sys.exit(1)
         elif opcode == op["jze"]:
             if wordmem[ acc_adr] == 0:
                 if operand == pc -1:
@@ -271,7 +380,6 @@ def emulate ( filename, nolisting ) :
             (quotient, remainder ) = ( dividend//divisor, dividend % divisor )
             wordmem[reg["Q"]] = quotient & 0xFFFFFF
             wordmem[acc_adr] = remainder
-
             # print ( "DIV %d / %d = %d REM %d" % ( dividend, divisor, quotient, remainder))
 
         elif opcode == op["out"]:
@@ -287,6 +395,8 @@ def emulate ( filename, nolisting ) :
         else:
             print ("Error - unidentified opcode 0x%02x" % opcode)
 
+    if machine == 500 :
+        print_exec_time(timers)
     if not nolisting:
         print ( ("").join(conout) )
 
@@ -296,8 +406,9 @@ if __name__ == "__main__":
     """
     filename = ""
     nolisting = False
+    machine = 500
     try:
-        opts, args = getopt.getopt( sys.argv[1:], "f:nh", ["filename=","nolisting","help"])
+        opts, args = getopt.getopt( sys.argv[1:], "f:1:4:5:nh", ["filename=","100","400","500","nolisting","help"])
     except getopt.GetoptError as  err:
         print(err)
         usage()
@@ -307,12 +418,18 @@ if __name__ == "__main__":
             filename = arg
         elif opt in ("-n", "--nolisting" ) :
             nolisting = True
+        elif opt in ("-1", "--100" ) :
+            machine = 100
+        elif opt in ("-4", "--400" ) :
+            machine = 400
+        elif opt in ("-4", "--500" ) :
+            machine = 500
         elif opt in ("-h", "--help" ) :
             usage()
         else:
             sys.exit(1)
 
     if filename != "":
-        emulate( filename , nolisting)
+        emulate( filename , nolisting, machine)
     else:
         usage()
